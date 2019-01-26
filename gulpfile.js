7fa55682877fea46bb6cb79b1892606243de3ab5
Dmitry Rakov <rakov.di@gmail.com>
2019-01-26 20:16:06 +0300

    Changed gulpfile and rebuild all projects

--- gulpfile.js
+++ gulpfile.js
@@ -37,13 +37,19 @@ gulp.task('default', function(){
   console.log('It is alive!!!');
 });
 
+
+
+// ===========================
+// PORTFOLIO - CHORDS
+// ===========================
+
 // Clean
-gulp.task('clean', function(){
+gulp.task('chords_clean', function(){
   return del('./myportfolio.local/portfolio/chords');
 });
 
 // HTML
-gulp.task('html', function(){
+gulp.task('chords_html', function(){
   return gulp.src('./src/portfolio/chords/**/*.html')
     // .pipe(concat('index.html'))
     // .pipe(newer('./myportfolio.local/portfolio/chords'))
@@ -53,14 +59,14 @@ gulp.task('html', function(){
     .pipe(gulp.dest('./myportfolio.local/portfolio/chords'));
 });
 
-gulp.task('pug', function(){
+gulp.task('chords_pug', function(){
   return gulp.src('./src/portfolio/chords/**/*.pug', {base: 'src'})
     .pipe(pug())
     .pipe(gulp.dest('./myportfolio.local/portfolio/chords'));
 });
 
 // Images
-gulp.task('img', function(){
+gulp.task('chords_img', function(){
   return gulp.src(['./src/portfolio/chords/img/**/*', '!./src/portfolio/chords/img/sprite_png/**/*', '!./src/portfolio/chords/img/sprite_png', '!./src/portfolio/chords/img/sprite_svg/**/*', '!./src/portfolio/chords/img/sprite_svg'])
     // .pipe(newer('./myportfolio.local/portfolio/chords/img'))
     // .pipe(imagemin({
@@ -70,7 +76,7 @@ gulp.task('img', function(){
 });
 
 // Sprite
-gulp.task('sprite_png', function () {
+gulp.task('chords_sprite_png', function () {
   var spriteData = gulp.src('./src/portfolio/chords/img/sprite_png/**/*.{png,jpg,jpeg}')
     .pipe(spritesmith({
       imgName: 'sprite.png',
@@ -116,7 +122,7 @@ var config = {
   }
 };
 
-gulp.task('sprite_svg', function () {
+gulp.task('chords_sprite_svg', function () {
   var spriteData = gulp.src('./src/portfolio/chords/img/sprite_svg/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./src/portfolio/chords/img/sprite_svg'));
@@ -125,7 +131,7 @@ gulp.task('sprite_svg', function () {
 });
 
 // Styles
-gulp.task('sass', function(){
+gulp.task('chords_sass', function(){
   return gulp.src('./src/portfolio/chords/**/*.scss')
     .on('data', function(file) {
       console.log(file);
@@ -139,7 +145,7 @@ gulp.task('sass', function(){
     .pipe(gulp.dest('./myportfolio.local/portfolio/chords'));
 });
 
-gulp.task('css', function(){
+gulp.task('chords_css', function(){
   return gulp.src('./src/portfolio/chords/**/*.css')
     .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 9'))
     .pipe(gulp.dest('./myportfolio.local/portfolio/chords'));
@@ -167,7 +173,7 @@ gulp.task('css', function(){
 // });
 
 // Scripts
-gulp.task('scripts', function(){
+gulp.task('chords_scripts', function(){
   return gulp.src('./src/portfolio/chords/js/**/*.js')
     // .pipe(newer('./myportfolio.local/portfolio/chords/js'))
     // .pipe(eslint())
@@ -195,22 +201,22 @@ gulp.task('scripts', function(){
 
 
 // complicated tasks
-gulp.task('build', function(){
-  runSequence('clean', 'html', 'sass', 'scripts', 'img');
+gulp.task('chords_build', function(){
+  runSequence('chords_clean', 'chords_html', 'chords_sass', 'chords_scripts', 'chords_img');
   // runSequence('clean', 'html', 'assets', 'sass', 'scripts');
 });
 
-gulp.task('watch', function() {
+gulp.task('chords_watch', function() {
   // gulp.watch('./src/portfolio/chords/**/*.pug', ['pug']);
-  gulp.watch('./src/portfolio/chords/**/*.html', ['html']);
+  gulp.watch('./src/portfolio/chords/**/*.html', ['chords_html']);
   // gulp.watch('./src/portfolio/chords/img/strite_png/**/*.{png,jpg,jpeg}', ['sprite_png']);
-  gulp.watch('./src/portfolio/chords/img/**/*.*', ['img']);
-  gulp.watch('./src/portfolio/chords/styles/**/*.scss', ['sass']);
+  gulp.watch('./src/portfolio/chords/img/**/*.*', ['chords_img']);
+  gulp.watch('./src/portfolio/chords/styles/**/*.scss', ['chords_sass']);
   // gulp.watch('./src/portfolio/chords/styles/**/*.css', ['css']);
-  gulp.watch('./src/portfolio/chords/js/**/*.js', ['scripts']);
+  gulp.watch('./src/portfolio/chords/js/**/*.js', ['chords_scripts']);
 });
 
-gulp.task('serve', function(){
+gulp.task('chords_serve', function(){
   browserSync.init({
     server: './myportfolio.local/portfolio/chords'
   });
@@ -218,52 +224,274 @@ gulp.task('serve', function(){
   browserSync.watch('./myportfolio.local/portfolio/chords/**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task('devloh', function(){
-  runSequence('build', 'watch');
+gulp.task('chords_devloh', function(){
+  runSequence('chords_build', 'chords_watch');
 });
 
-gulp.task('dev', function(){
-  runSequence('build', ['watch', 'serve']);
+gulp.task('chords_dev', function(){
+  runSequence('chords_build', ['chords_watch', 'chords_serve']);
 });
 
-// gulp.task('dev',
-//   gulp.series('build', gulp.parallel('watch', 'serve'))
-// );
-
-// ===================
-// DEPLOY TASKS
-// ===================
-
-// // Make archive
-// gulp.task('archive', function() {
-//   return gulp.src('./myportfolio.local/portfolio/chords/**/*')
-//     // .pipe(del('./archive.tar.gz'))
-//     .pipe(tar('archive.tar'))
-//     .pipe(gzip())
-//     .pipe(gulp.dest('.'))
-//   });
-
-// // Upload files to remote server
-// gulp.task('ftp', function() {
-//   return gulp.src("./archive.tar.gz")
-//     .pipe(ftp({
-//       host: "home.twinpro.ru",
-//       user: "twinpro",
-//       pass: "ip7ZBTB9",
-//       remotePath: "/mysite"
-//     }));
-// });
-
-// gulp.task('sftp', function() {
-//   return gulp.src("./archive.tar.gz")
-//     .pipe(sftp({
-//       host: "home.twinpro.ru",
-//       user: "twinpro",
-//       pass: "ip7ZBTB9"
-//     }));
-// });
 
 
+// ===========================
+// PORTFOLIO - WORDS
+// ===========================
 
-// }
-// );
+// Clean
+gulp.task('words_clean', function(){
+  return del('./myportfolio.local/portfolio/words');
+});
+
+// HTML
+gulp.task('words_html', function(){
+  return gulp.src('./src/portfolio/words/**/*.html')
+    .pipe(gulp.dest('./myportfolio.local/portfolio/words'));
+});
+
+gulp.task('words_pug', function(){
+  return gulp.src('./src/portfolio/words/**/*.pug', {base: 'src'})
+    .pipe(pug())
+    .pipe(gulp.dest('./myportfolio.local/portfolio/words'));
+});
+
+// Images
+gulp.task('words_img', function(){
+  return gulp.src(['./src/portfolio/words/img/**/*', '!./src/portfolio/words/img/sprite_png/**/*', '!./src/portfolio/words/img/sprite_png', '!./src/portfolio/words/img/sprite_svg/**/*', '!./src/portfolio/words/img/sprite_svg'])
+    .pipe(gulp.dest('./myportfolio.local/portfolio/words/img'));
+});
+
+// Sprite
+gulp.task('words_sprite_png', function () {
+  var spriteData = gulp.src('./src/portfolio/words/img/sprite_png/**/*.{png,jpg,jpeg}')
+    .pipe(spritesmith({
+      imgName: 'sprite.png',
+      cssName: 'sprite.css',
+      cssFormat: 'css',
+      algorithm: 'top-down',
+      padding: 0,
+      algorithmOpts: {sort: false}
+    }));
+
+  spriteData.img
+    .pipe(gulp.dest('./src/portfolio/words/img'));
+
+  spriteData.css
+    .pipe(gulp.dest('./src/portfolio/words/img/sprite_png'));
+
+  return spriteData;
+});
+
+var config = {
+  shape : {
+    spacing : {              // Spacing related options
+      padding : [0,0,0,0]    // Padding around all shapes
+    }
+  },
+  mode : {
+    css : {                   // Activate the «css» mode
+      layout: 'vertical',
+      render      : {
+        css     : true        // Activate CSS output (with default options)
+      },
+      dest : './',
+      sprite: '../sprite.svg',
+      bust: false,
+      dimensions: {
+        extra: true
+      }
+    }
+  }
+};
+
+gulp.task('words_sprite_svg', function () {
+  var spriteData = gulp.src('./src/portfolio/words/img/sprite_svg/**/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('./src/portfolio/words/img/sprite_svg'));
+
+  return spriteData;
+});
+
+// Styles
+gulp.task('words_sass', function(){
+  return gulp.src('./src/portfolio/words/**/*.scss')
+    .on('data', function(file) {
+      console.log(file);
+    })
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions', 'ie 11'))
+    .pipe(gulpIf(!isDevelopment, combine(
+      cssnano(),
+      rename({suffix: '.min'}) ))
+    )
+    .pipe(gulp.dest('./myportfolio.local/portfolio/words'));
+});
+
+gulp.task('words_css', function(){
+  return gulp.src('./src/portfolio/words/**/*.css')
+    .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 9'))
+    .pipe(gulp.dest('./myportfolio.local/portfolio/words'));
+});
+
+// Scripts
+gulp.task('words_scripts', function(){
+  return gulp.src('./src/portfolio/words/js/**/*.js')
+    .pipe(webpackStream( require('./webpack.config.js') ))
+    .pipe(gulpIf(!isDevelopment, combine(
+      uglify(),
+      rename({suffix: '.min'})
+    )))
+    .pipe(gulp.dest('./myportfolio.local/portfolio/words/js'));
+});
+
+// complicated tasks
+gulp.task('words_build', function(){
+  runSequence('words_clean', 'words_html', 'words_sass', 'words_scripts', 'words_img');
+  // runSequence('clean', 'html', 'assets', 'sass', 'scripts');
+});
+
+gulp.task('words_watch', function() {
+  // gulp.watch('./src/portfolio/words/**/*.pug', ['pug']);
+  gulp.watch('./src/portfolio/words/**/*.html', ['words_html']);
+  // gulp.watch('./src/portfolio/words/img/strite_png/**/*.{png,jpg,jpeg}', ['sprite_png']);
+  gulp.watch('./src/portfolio/words/img/**/*.*', ['words_img']);
+  gulp.watch('./src/portfolio/words/styles/**/*.scss', ['words_sass']);
+  // gulp.watch('./src/portfolio/words/styles/**/*.css', ['css']);
+  gulp.watch('./src/portfolio/words/js/**/*.js', ['words_scripts']);
+});
+
+gulp.task('words_serve', function(){
+  browserSync.init({
+    server: './myportfolio.local/portfolio/words'
+  });
+
+  browserSync.watch('./myportfolio.local/portfolio/words/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('words_devloh', function(){
+  runSequence('words_build', 'words_watch');
+});
+
+gulp.task('words_dev', function(){
+  runSequence('words_build', ['words_watch', 'words_serve']);
+});
+
+
+// ===========================
+// PORTFOLIO - MAIN PAGE
+// ===========================
+
+// Clean
+gulp.task('main_clean', function(){
+  return del('./myportfolio.local/**/*','!./myportfolio.local/{portfolio,portfolio/**/*}');
+});
+
+// HTML
+gulp.task('main_html', function(){
+  return gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('./myportfolio.local'));
+});
+
+// PHP
+gulp.task('main_php', function(){
+  return gulp.src('./src/**/*.php')
+    .pipe(gulp.dest('./myportfolio.local'));
+});
+
+// Fonts
+gulp.task('main_fonts', function(){
+  return gulp.src('./src/fonts/**/*')
+    .pipe(gulp.dest('./myportfolio.local/fonts'));
+});
+
+// Media
+gulp.task('main_media', function(){
+  return gulp.src('./src/audio/**/*')
+    .pipe(gulp.dest('./myportfolio.local/audio'));
+});
+
+// Images
+gulp.task('main_img', function(){
+  return gulp.src(['./src/img/**/*', '!./src/img/{icons_big,icons_big/**/*}'])
+    .pipe(gulp.dest('./myportfolio.local/img'));
+});
+
+// Styles
+gulp.task('main_sass', function(){
+  return gulp.src('./src/css/**/*.scss')
+    .pipe(sass({
+      includePaths: require('node-normalize-scss').includePaths
+    }))
+    .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 11'))
+    .pipe(gulpIf(!isDevelopment, combine(
+      cssnano(),
+      rename({suffix: '.min'}) ))
+    )
+    .pipe(gulp.dest('./myportfolio.local/css'));
+});
+
+// Scripts
+gulp.task('main_scripts', function(){
+  return gulp.src('./src/js/**/*.js')
+    .pipe(gulpIf(!isDevelopment, combine(
+      uglify(),
+      rename({suffix: '.min'})
+    )))
+    .pipe(gulp.dest('./myportfolio.local/js'));
+});
+
+
+// complicated tasks
+gulp.task('main_build', function(){
+  runSequence('main_clean', 'main_html', 'main_php', 'main_fonts', 'main_sass', 'main_img', 'main_media', 'main_scripts');
+});
+
+gulp.task('main_watch', function() {
+  // gulp.watch('./src/portfolio/chords/**/*.pug', ['pug']);
+  gulp.watch('./src/portfolio/chords/**/*.html', ['main_html']);
+  gulp.watch('./src/portfolio/chords/**/*.php', ['main_php']);
+  gulp.watch('./src/portfolio/chords/styles/**/*.scss', ['main_sass']);
+  gulp.watch('./src/portfolio/chords/img/**/*.*', ['main_img']);
+  gulp.watch('./src/portfolio/chords/js/**/*.js', ['main_scripts']);
+});
+
+gulp.task('main_serve', function(){
+  browserSync.init({
+    server: './myportfolio.local'
+  });
+
+  browserSync.watch('./myportfolio.local/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('main_dev', function(){
+  runSequence('main_build', ['main_watch', 'main_serve']);
+});
+
+// ===========================
+// PORTFOLIO - OTHER
+// ===========================
+
+// Clean
+gulp.task('other_clean', function(){
+  return del('./myportfolio.local/portfolio/**/*','!./myportfolio.local/portfolio/{words,words/**,words/**/*,chords,chords/**,chords/**/*}');
+});
+
+// Files
+gulp.task('other_files', function(){
+  return gulp.src(['./src/portfolio/**/*','!./src/portfolio/{words,words/**,words/**/*,chords,chords/**,chords/**/*}'])
+    .pipe(gulp.dest('./myportfolio.local/portfolio'));
+});
+
+// complicated tasks
+gulp.task('other_build', function(){
+  runSequence('other_clean', 'other_files');
+});
+
+// ===========================
+// PORTFOLIO - ALL (except CHORDS and WORDS) Webpack for them
+// ===========================
+
+// complicated tasks
+gulp.task('all_build', function(){
+  runSequence('main_build', 'other_build');
+});
